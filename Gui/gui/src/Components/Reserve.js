71a20822702e1a7fdffab672a180973617c3e553
Islamwael-1999 <islam.wael1999@gmail.com>
2022-01-06 06:44:04 +0200

    Search Modification

--- Gui/gui/src/Components/Reserve.js
+++ Gui/gui/src/Components/Reserve.js
@@ -1,75 +1,84 @@
-import React, { Component ,useState} from "react";
+import React, { Component, useState } from "react";
 import data from "./data";
 
-class Reserve extends Component{
-    constructor(props)
-    {
+
+class Reserve extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-        filter:''
+        this.state = {
+            filter: ''
         }
     }
-render(){
-    data.cardData[0].title="HI";
-    
-    data.cardData[0].Availability = 'Out of Service';
-    const searchText = (event) => {
+    searchText = (event) => {
         this.setState({
-            filter:event.target.value          
+            filter: event.target.value
         });
-        
     }
-    let dataSearch = data.cardData.filter(item => {
-        return Object.keys(item).some(key =>
-            item[key].toString().toLowerCase().includes(this.state.filter.toString().toLowerCase())
-        )
-        
-    })
-    return (
-        <section className="py-4 container">
-            <div className="row justify-content-center">
-                <div className="col-12 mb-5">
-                    <div className="mb-3 col-4 mx-auto">
-                        <label className="form-label h4">Reserve</label>
-                        <input
-                            type="text"
-                            className="from-control"
-                            value={this.state.filter}
-                            onChange={searchText.bind(this)}
-                        />
+    render() {
+
+        const excludeColumns = ["price", "Availability"];
+        let dataSearch = data.cardData.filter(item => {
+            return Object.keys(item).some(key =>
+                excludeColumns.includes(key) ? item[key].toString().toLowerCase().includes(this.state.filter.toString().toLowerCase()) : false
+            )
+
+        })
+        // {{textAlign:'left', marginLeft: '10px',paddingLeft:'0px'}}
+        console.log(dataSearch)
+        return (
+            <section className="py-4 container">
+                <div className="row justify-content-center">
+                    <div className="col-12 mb-5">
+                        <div className="mb-3 col-4  text-left" >
+                            <label className="form-label h4 " >Reserve</label>
+                            <input
+                                type="text"
+                                className="mb-3 col-10"
+                                value={this.state.filter}
+                                onChange={this.searchText.bind(this)}
+                                style={{ textAlign: 'left' }}
+                            />
+                            <select className="mb-3 col-10 row-5 " id="selectList">
+                            <option value="option 1">id</option>
+                            <option value="option 2">title</option>
+                            <option value="option 2">price</option>
+                            <option value="option 2">Status</option>
+                            
+                        </select>
+                        </div>
+                        
                     </div>
-                </div>
-                {dataSearch.map((item, index) => {
-                    return (
-                        <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
-                            <div className="card p-0 overflow-hidden h-100 shadow">
-                                <img src={item.img} className="card-img-top" />
-                                <div className="card-body">
-                                    <h5 className="card-title">{item.title}</h5>
-                                    <p className="card-text"> {item.price}</p>
-                                    <p className="card-text"> {item.Availability}</p>
-                                    {item.Availability === 'Out of Service' &&
+                    {dataSearch.map((item, index) => {
+                        return (
+                            <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+                                <div className="card p-0 overflow-hidden h-100 shadow">
+                                    <img src={item.img} className="card-img-top" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{item.title}</h5>
+                                        <p className="card-text"> {item.price}</p>
+                                        <p className="card-text"> {item.Availability}</p>
+                                        {item.Availability === 'Out of Service' &&
 
-                                        <button type='button' className="btn btn-primary" disabled>Reserve</button>
-                                    }
-                                    {item.Availability === 'Active' &&
-                                        <button type='button' className="btn btn-primary" >Reserve</button>
-                                    }
-                                
+                                            <button type='button' className="btn btn-primary" disabled>Reserve</button>
+                                        }
+                                        {item.Availability === 'Active' &&
+                                            <button type='button' className="btn btn-primary" >Reserve</button>
+                                        }
 
+
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </div>
-        </section>
-    )
+                        )
+                    })}
+                </div>
+            </section>
+        )
 
 
 
 
-}
+    }
 
 
 }
--- Gui/gui/src/Components/data.js
+++ Gui/gui/src/Components/data.js
@@ -10,35 +10,35 @@ var data = {
             id:1,
             img: car1,
             title: 'Elantra',
-            price: '5.99$/hour',
+            price: '5.99$',
             Availability: 'Active'
         },
         {
             id:2,
             img: car2,
             title: 'Urus',
-            price: '20.99$/hour',
+            price: '20.99$',
             Availability: 'Out of Service'
         },
         {
             id:3,
             img: car3,
             title: 'Corvette 2020',
-            price: '15.99$/hour',
+            price: '15.99$',
             Availability: 'Active'
         },
         {
             id:4,
             img: car4,
             title: 'Corvette 2016',
-            price: '12.99$/hour',
+            price: '12.99$',
             Availability:'Out of Service'
         },
         {
             id:5,
             img: car5,
             title: 'Honda',
-            price: '8.99$/hour',
+            price: '8.99$',
             Availability: 'Active'
         },
     ]
